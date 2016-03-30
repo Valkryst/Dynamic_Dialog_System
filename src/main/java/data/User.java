7b94abea01a4389eea1eab6229003cd2f8c9af21
Valkryst <valkryst@valkryst.com>
2016-03-30 01:04:39 -0300

    Added a lock for the splay tree, rewrite the getContextByName method to use the lock, generated new serialVersionUID.

--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -3,15 +3,19 @@ package data;
 import data.collection.SplayTree;
 
 import java.io.Serializable;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class User implements Serializable {
-    private static final long serialVersionUID = 8231393811475248767L;
+    private static final long serialVersionUID = 7804586155533229144L;
 
     /** A unique ID. */
     private final long id;
 
     /** The SplayTree containing all currently loaded Contexts, with their Names as Keys. */
     private SplayTree<String, Context> splayTree_context = new SplayTree<>();
+
+    /** The Lock of the splayTree_context data structure. */
+    private ReentrantReadWriteLock lock_splayTree_context = new ReentrantReadWriteLock();
 
     /**
      * Construct a new User.
@@ -44,6 +48,17 @@ public class User implements Serializable {
      *         Else NULL is returned.
      */
     public Context getContextByName(final String contextName) {
-        return splayTree_context.get(contextName);
+        lock_splayTree_context.readLock().lock();
+
+        final Context context = splayTree_context.get(contextName);
+
+        lock_splayTree_context.readLock().unlock();
+
+        return context;
+    }
+
+    /** @return The Lock of the splayTree_context data structure. */
+    public ReentrantReadWriteLock getLock_splayTree_context() {
+        return lock_splayTree_context;
     }
 }
