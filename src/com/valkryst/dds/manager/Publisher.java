46339ea6da373cff5962ef9b288c5e117220ee3d
Valkryst <valkryst@valkryst.com>
2016-06-04 19:47:40 -0300

    Rewrote the majority of the way the publisher works.
    To add a subscriber, you now need to specify the response type and the subscriber.
    
    With the new ArrayListMultimap storing all of the response type + subscriber pairs, it's much easier to notify only those subscribers who actually can respond to whatever response is being sent out.

--- src/com/valkryst/dds/manager/Publisher.java
+++ src/com/valkryst/dds/manager/Publisher.java
@@ -1,16 +1,16 @@
 package com.valkryst.dds.manager;
 
+import com.google.common.collect.ArrayListMultimap;
 import com.valkryst.dds.object.Response;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Publisher implements Serializable {
     private static final long serialVersionUID = 9075221020153629524L;
 
     /** The ArrayList of all objects to be notified. */
-    private ArrayList<Notifiable> arrayList_responseSubscribers = new ArrayList<>();
+    private ArrayListMultimap<String, Notifiable> arrayListMultimap_responseSubscribers = ArrayListMultimap.create();
 
     /**
      * Publishes the specified Responses to all Response subscribers.
@@ -24,8 +24,11 @@ public class Publisher implements Serializable {
     void publishResponses(final DDSManager ddsManager, final List<Response> responses) {
         responses.parallelStream()
                 .forEach(response -> {
-                    arrayList_responseSubscribers.parallelStream()
-                            .forEach(subscriber -> subscriber.handleResponse(ddsManager, response));
+                    final String responseType = response.getResponseType();
+
+                    arrayListMultimap_responseSubscribers.get(responseType)
+                                                         .parallelStream()
+                                                         .forEach(subscriber -> subscriber.handleResponse(ddsManager, response));
                 });
     }
 
@@ -34,24 +37,36 @@ public class Publisher implements Serializable {
      *
      * Duplicate entries will be ignored.
      *
+     * @param responseType
+     *         The type of response to subscribe to.
      *
      * @param subscriber
      *         The Notifiable subscriber to add into the Dynamic Dialog System.
      */
-    public void addResponseSubscriber(final Notifiable subscriber) {
-        if(! arrayList_responseSubscribers.contains(subscriber)) {
-            arrayList_responseSubscribers.add(subscriber);
+    public void addResponseSubscriber(final String responseType, final Notifiable subscriber) {
+        /*
+         * If the subscriber has not previously subscribed to
+         * the specified response type, then allow it to
+         * subscribe.
+         */
+        boolean isAlreadySubscribed = arrayListMultimap_responseSubscribers.get(responseType)
+                                                                           .contains(subscriber);
+
+        if(! isAlreadySubscribed) {
+            arrayListMultimap_responseSubscribers.put(responseType, subscriber);
         }
     }
 
     /**
      * Removes the specified Notifable subscriber from the Dynamic Dialog System.
      *
+     * @param responseType
+     *         The type of response that the subscriber is subscribed to to.
      *
      * @param subscriber
      *         The Notifiable subscriber to remove from the Dynamic Dialog System.
      */
-    public void removeResponseSubscriber(final Notifiable subscriber) {
-        arrayList_responseSubscribers.remove(subscriber);
+    public void removeResponseSubscriber(final String responseType, final Notifiable subscriber) {
+        arrayListMultimap_responseSubscribers.remove(responseType, subscriber);
     }
 }
